68dec762fd020ba312bac6b3fe0be46188ce1e25
orun028 <sydney7754@gmail.com>
2023-04-26 15:12:46 +0700

    up

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 const conn = require('./db');
-const timeout = require('connect-timeout')
 const morgan = require('morgan')
 const cors = require('cors')
 const passport = require('passport')
@@ -19,23 +18,21 @@ const sess = {
   saveUninitialized: true,
   secret: SESSION_SECRET,
   cookie: {
-    maxAge: 1000 * 30/* 60 * 60 * 24 */
+    maxAge: 60 * 60 * 24
   },
   store: sessionStore
 }
 
 if (app.get('env') === 'production') {
-  app.set('trust proxy', 1) // trust first proxy
-  sess.cookie.secure = true // serve secure cookies
+  app.set('trust proxy', 1)
+  sess.cookie.secure = true 
 }
 
 app.disable('x-powered-by');
-// app.use(timeout('5s'))
 app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-// app.use(haltOnTimedout)
 app.use(session(sess))
 app.use(passport.initialize())
 app.use(passport.session())
